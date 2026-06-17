959b1846300fd5019288dfe75e89831f06dd3696
ferreterialasiciliana-spec <ferreterialasiciliana@gmail.com>
2026-06-17 12:51:10 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'faltantes-v5';
+const CACHE = 'faltantes-v6';
 const ARCHIVOS = ['/index.html', '/pedido.html', '/transito.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
